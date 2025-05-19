2e0d349429a1d5d35c24f218410b277fec560e47
noslidap <denilson.padilha.morais@gmail.com>
2025-05-19 11:34:12 -0300

    Update product images to show desserts

--- src/data/products.ts
+++ src/data/products.ts
@@ -5,7 +5,7 @@ export const products: Product[] = [
     id: 'traditional',
     name: 'Pudim Tradicional',
     description: 'Nosso clássico pudim de leite condensado, suave e com calda caramelizada perfeita.',
-    image: 'https://images.pexels.com/photos/4099237/pexels-photo-4099237.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: 'https://images.pexels.com/photos/6341564/pexels-photo-6341564.jpeg',
     prices: {
       '80ml': 8.00,
       '120ml': 10.00,
@@ -19,7 +19,7 @@ export const products: Product[] = [
     id: 'chocolate',
     name: 'Pudim de Chocolate',
     description: 'Irresistível pudim de chocolate cremoso, coberto com calda de chocolate.',
-    image: 'https://images.pexels.com/photos/2144112/pexels-photo-2144112.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: 'https://images.pexels.com/photos/4110541/pexels-photo-4110541.jpeg',
     prices: {
       '80ml': 9.00,
       '120ml': 12.50,
@@ -32,7 +32,7 @@ export const products: Product[] = [
     id: 'brigadeiro',
     name: 'Pudim de Brigadeiro',
     description: 'A combinação perfeita do nosso pudim com o sabor do tradicional brigadeiro brasileiro.',
-    image: 'https://images.pexels.com/photos/2373520/pexels-photo-2373520.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: 'https://images.pexels.com/photos/6341563/pexels-photo-6341563.jpeg',
     prices: {
       '80ml': 9.50,
       '120ml': 13.00,
@@ -45,7 +45,7 @@ export const products: Product[] = [
     id: 'doce-de-leite',
     name: 'Pudim de Doce de Leite',
     description: 'Pudim com o sabor intenso e caramelizado do autêntico doce de leite.',
-    image: 'https://images.pexels.com/photos/5702773/pexels-photo-5702773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: 'https://images.pexels.com/photos/6341565/pexels-photo-6341565.jpeg',
     prices: {
       '80ml': 9.50,
       '120ml': 13.00,
@@ -58,7 +58,7 @@ export const products: Product[] = [
     id: 'pacoca',
     name: 'Pudim de Paçoca',
     description: 'Delicioso pudim com o sabor inconfundível da paçoca brasileira.',
-    image: 'https://images.pexels.com/photos/2067396/pexels-photo-2067396.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: 'https://images.pexels.com/photos/6341566/pexels-photo-6341566.jpeg',
     prices: {
       '80ml': 9.50,
       '120ml': 13.00,
@@ -71,7 +71,7 @@ export const products: Product[] = [
     id: 'chocolate-branco',
     name: 'Pudim de Chocolate Branco',
     description: 'Pudim de chocolate branco, suave e sofisticado.',
-    image: 'https://images.pexels.com/photos/2693448/pexels-photo-2693448.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: 'https://images.pexels.com/photos/6341567/pexels-photo-6341567.jpeg',
     prices: {
       '80ml': 9.50,
       '120ml': 13.00,
@@ -84,7 +84,7 @@ export const products: Product[] = [
     id: 'chocolate-suico',
     name: 'Pudim de Chocolate Suíço',
     description: 'Pudim especial inspirado no chocolate suíço, com textura extra cremosa.',
-    image: 'https://images.pexels.com/photos/2680603/pexels-photo-2680603.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: 'https://images.pexels.com/photos/6341568/pexels-photo-6341568.jpeg',
     prices: {
       '80ml': 11.50,
       '120ml': 14.90,
@@ -98,7 +98,7 @@ export const products: Product[] = [
     id: 'ninho-nutella',
     name: 'Pudim Ninho com Nutella',
     description: 'A combinação irresistível do leite Ninho com Nutella em um pudim cremoso.',
-    image: 'https://images.pexels.com/photos/4553127/pexels-photo-4553127.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: 'https://images.pexels.com/photos/6341569/pexels-photo-6341569.jpeg',
     prices: {
       '80ml': 12.90,
       '120ml': 14.90,
@@ -112,7 +112,7 @@ export const products: Product[] = [
     id: 'pistache',
     name: 'Pudim de Pistache',
     description: 'Pudim sofisticado com o sabor único do pistache, finalizado com pistaches picados.',
-    image: 'https://images.pexels.com/photos/1854652/pexels-photo-1854652.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: 'https://images.pexels.com/photos/6341570/pexels-photo-6341570.jpeg',
     prices: {
       '80ml': 12.90,
       '120ml': 14.90,
