56fa1b0632ac05eb2d034d64912cd7616733dbc3
noslidap <denilson.padilha.morais@gmail.com>
2025-05-26 00:31:57 -0300

    Connect to Supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
@@ -13,27 +13,29 @@ import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
-    <CartProvider>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={
-              <>
-                <Hero />
-                <Menu />
-                <About />
-                <Contact />
-              </>
-            } />
-            <Route path="/admin" element={<AdminPage />} />
-          </Routes>
-        </main>
-        <Cart />
-        <FloatingWhatsApp />
-        <Footer />
-      </div>
-    </CartProvider>
+    <BrowserRouter>
+      <CartProvider>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={
+                <>
+                  <Hero />
+                  <Menu />
+                  <About />
+                  <Contact />
+                </>
+              } />
+              <Route path="/admin" element={<AdminPage />} />
+            </Routes>
+          </main>
+          <Cart />
+          <FloatingWhatsApp />
+          <Footer />
+        </div>
+      </CartProvider>
+    </BrowserRouter>
   );
 }
 
